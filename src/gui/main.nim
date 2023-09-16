ae65e811cbdc77f0b2e59440687149e3aae43563
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2023-09-16 12:25:49 +0700

    experiment with Kool Fx™

--- src/gui/main.nim
+++ src/gui/main.nim
@@ -1,11 +1,21 @@
 import ../res/resources
 import wNim/[wApp, wFrame]
-
 import appFrame
+
+import winim/lean
 
 when isMainModule:
   let
     app = App(wSystemDpiAware)
     app_frm = MyAppFrame()
-  app_frm.show()
+
+  when defined(fancyEffects):
+    AnimateWindow(
+      app_frm.mHwnd,
+      500,
+      AW_ACTIVATE or AW_BLEND
+    )
+  else:
+    app_frm.show()
+
   app.mainLoop()
